a07c2b3b353e264113bd4429255c927acc0c8b2e
kamilgwozdziowski <kamil.gwozd@gmail.com>
2020-09-10 14:36:13 +0200

    fix integration tests

--- app/code/MylSoft/Weather/Test/Integration/Cron/GetWeatherTest.php
+++ app/code/MylSoft/Weather/Test/Integration/Cron/GetWeatherTest.php
@@ -12,6 +12,12 @@ class GetWeatherTest extends TestCase
     /** @var ObjectManagerInterface */
     private $objectManager;
 
+    /** @var Curl */
+    private $curlMock;
+
+    /** @var GetWeather */
+    private $object;
+
     /**
      * setUp
      *
@@ -21,48 +27,75 @@ class GetWeatherTest extends TestCase
     {
         parent::setUp();
         $this->objectManager = Bootstrap::getObjectManager();
+        $this->curlMock = $this->createMock(Curl::class);
+
+        $this->object = new GetWeather($this->objectManager, $this->curlMock);
     }
 
+    /**
+     * Test get city data API
+     *
+     * @return void
+     */
     public function testGetCityData()
     {
-        $curlMock = $this->getMockBuilder(Curl::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['getBody'])
-            ->getMock();
-
-        $curlMock->expects($this->once())
+        $this->curlMock->expects($this->once())
             ->method('getBody')
-            ->willReturn(json_encode([
-                'name' => 'test',
-                'id' => 1,
-                'cod' => 200
-            ]));
+            ->willReturn($this->getDefaultDataJson());
 
-        $object = new GetWeather($this->objectManager, $curlMock);
-        $response = $object->getCityData('Test', 'ts');
+        $response = $this->object->getCityData('Test', 'ts');
 
         $this->assertEquals('test', $response['name']);
         $this->assertEquals('1', $response['city_id']);
     }
 
-    public function testGetCityData404()
+    /**
+     * Test code back from API
+     * 
+     * @dataProvider dataPrivider404
+     */
+    public function testGetCityData404(int $code, bool $expect)
     {
-        $curlMock = $this->getMockBuilder(Curl::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['getBody'])
-            ->getMock();
-
-        $curlMock->expects($this->once())
+        $this->curlMock->expects($this->once())
             ->method('getBody')
-            ->willReturn(json_encode([
-                'name' => 'test',
-                'id' => 1,
-                'cod' => 404
+            ->willReturn($this->getDefaultDataJson([
+                'cod' =>  $code
             ]));
 
-        $object = new GetWeather($this->objectManager, $curlMock);
-        $response = $object->getCityData('Test', 'ts');
+        $response = $this->object->getCityData('Test', 'ts');
 
-        $this->assertEquals([], $response);
+        $this->assertEquals($expect, count($response) == 0);
+    }
+
+    /**
+     * Get default data to test
+     *
+     * @param array $params
+     * @return string
+     */
+    private function getDefaultDataJson(array $params = []): string
+    {
+        $arr = [
+            'name' => 'test',
+            'id' => 1,
+            'cod' => 200
+        ];
+
+        $arr = array_merge($arr, $params);
+        return json_encode($arr);
+    }
+
+    /**
+     * Data Provider
+     *
+     * @return void
+     */
+    public function dataPrivider404()
+    {
+        return [
+            [404, true],
+            [500, true],
+            [200, false]
+        ];
     }
 }
